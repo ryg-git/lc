dbcd777ba62167d9a23413df83d9a29050ce13c8
ryg-git <ygretharekar@outlook.com>
2023-08-30 16:42:21 +0530

    lc 20230830

--- solution.py
+++ solution.py
@@ -2,19 +2,31 @@ from bisect import bisect_left, bisect_right
 from collections import deque
 from typing import *
 
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        return None
+    def minimumReplacement(self, nums: List[int]) -> int:
+        l = len(nums) - 1
+        
+        las = nums[l]
+
+        ans = 0
+
+        for i in range(l - 1, -1, -1):
+            if nums[i] > las:
+                t = nums[i] // las
+                if nums[i] % las:
+                    t += 1
+                las = nums[i] // t
+                ans = t - 1
+            else:
+                las = nums[i]
+
+        return ans
+
 
 def main():
     s = Solution()
-    ans = s.canCross(stones=[0, 1, 3, 5, 6, 8, 12, 17])
+    ans = s.minimumReplacement(nums=[3, 9, 3])
     print(ans)
+
 
 main()
