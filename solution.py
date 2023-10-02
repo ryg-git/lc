0971387993c81882a788e43894202062111041bc
ryg-git <ygretharekar@outlook.com>
2023-10-02 19:54:03 +0530

    lc 20231002

--- solution.py
+++ solution.py
@@ -5,35 +5,51 @@ from typing import *
 
 
 class Solution:
-    def maximumSumOfHeights(self, maxHeights: List[int]) -> int:
+    def minSizeSubarray(self, nums: List[int], target: int) -> int:
+        sn = sum(nums)
+        n = len(nums)
 
-        def find_max(arr: List[int]):
-            st = [(arr[0], arr[0])]
-            n = len(arr)
+        d = target // sn
+        r = target % sn
 
-            for i in range(1, n):
-                cnt = 1
-                j = i
-                while j > 0 and arr[j - 1] > arr[j]:
-                    s, c = st[j]
-                    cnt += c
-                    j -= 1
+        def find_len(nu, tar, mm):
 
-                if st:
-                    su += st[-1][1]
+            ml = 100001 if mm else -1
 
-                st.append((h, su + cnt * h, cnt))
+            def check(nu, a):
+                return min(nu, a) if mm else max(nu, a)
 
-            return st
+            su, r, l = 0, 0, 0
 
-        le = [0] + list(map(lambda x: x[1], find_max(maxHeights)))
+            while l <= r and l < n:
+                if su == tar:
+                    ml = check(ml, r - l)
+                if r < n and su < tar:
+                    su += nu[r]
+                    r += 1
+                else:
+                    su -= nu[l]
+                    l += 1
 
-        return le
+            return ml
+
+        o = find_len(nums, r, True)
+        t = find_len(nums, sn - r, False)
+
+        if o == 100001 and t == -1:
+            return -1
+        elif o == 100001:
+            return d * n + (n - t)
+        elif t == -1:
+            return d * n + o
+        else:
+            return d * n + min(o, n - t)
 
 
 def main():
     s = Solution()
-    ans = s.maximumSumOfHeights(maxHeights=[3, 2, 5, 5, 2, 3])
+    ans = s.minSizeSubarray(
+        nums=[1, 4, 8, 5, 9, 8, 8, 2, 3, 1, 6, 2, 7, 5, 5, 3, 3, 5, 6], target=57)
     print(ans)
 
 
