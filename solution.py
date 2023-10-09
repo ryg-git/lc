91ad164a0ba21f1b9cc26c0cd4f81a8b57b14d4c
ryg-git <ygretharekar@outlook.com>
2023-10-09 16:26:46 +0530

    lc 20231009

--- solution.py
+++ solution.py
@@ -1,55 +1,43 @@
 from bisect import bisect_left, bisect_right
 from collections import defaultdict, deque
+from functools import cache
 import heapq
 from typing import *
 
 
 class Solution:
-    def minSizeSubarray(self, nums: List[int], target: int) -> int:
-        sn = sum(nums)
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
         n = len(nums)
 
-        d = target // sn
-        r = target % sn
+        def findInd(lorr):
+            l, r = 0, n
+            while l < r:
+                mid = (l + r) // 2
 
-        def find_len(nu, tar, mm):
-
-            ml = 100001 if mm else -1
-
-            def check(nu, a):
-                return min(nu, a) if mm else max(nu, a)
-
-            su, r, l = 0, 0, 0
-
-            while l <= r and l < n:
-                if su == tar:
-                    ml = check(ml, r - l)
-                if r < n and su < tar:
-                    su += nu[r]
-                    r += 1
+                if nums[mid] == target:
+                    if lorr:
+                        if mid == 0 or nums[mid - 1] < target:
+                            return mid
+                        else:
+                            r = mid
+                    else:
+                        if mid == n - 1 or nums[mid + 1] > target:
+                            return mid
+                        else:
+                            l = mid + 1
+                elif nums[mid] > target:
+                    r = mid
                 else:
-                    su -= nu[l]
-                    l += 1
+                    l = mid + 1
 
-            return ml
-
-        o = find_len(nums, r, True)
-        t = find_len(nums, sn - r, False)
-
-        if o == 100001 and t == -1:
             return -1
-        elif o == 100001:
-            return d * n + (n - t)
-        elif t == -1:
-            return d * n + o
-        else:
-            return d * n + min(o, n - t)
+
+        return [findInd(True), findInd(False)]
 
 
 def main():
     s = Solution()
-    ans = s.minSizeSubarray(
-        nums=[1, 4, 8, 5, 9, 8, 8, 2, 3, 1, 6, 2, 7, 5, 5, 3, 3, 5, 6], target=57)
+    ans = s.searchRange(nums=[2, 2], target=2)
     print(ans)
 
 
