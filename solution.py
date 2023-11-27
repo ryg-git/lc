fba66e86d5a370131e2e75dd82f1aee495b88226
ryg-git <ygretharekar@outlook.com>
2023-11-27 11:24:56 +0530

    LC 20231127

--- solution.py
+++ solution.py
@@ -1,32 +1,52 @@
 from bisect import bisect_left, bisect_right
 from collections import defaultdict, deque, Counter
-from functools import cache
+from functools import cache, lru_cache
 import heapq
 from typing import *
 
 
 class Solution:
-    def kthGrammar(self, n: int, k: int) -> int:
-        if n == 1:
-            return 0
+    def knightDialer(self, n: int) -> int:
+        moves = [
+            (2, 1),
+            (2, -1),
+            (-2, 1),
+            (-2, -1),
+            (1, 2),
+            (1, -2),
+            (-1, -2),
+            (-1, 2),
+        ]
 
-        def rec(nn: int, kk: int):
-            if nn == 1:
-                return 0
-
-            mid = nn // 2
-
-            if mid >= kk:
-                return rec(mid, kk)
+        @lru_cache(None)
+        def helper(n, r, c):
+            if n == 0:
+                return 1
             else:
-                return 1 - rec(mid, kk - mid)
+                ans = 0
 
-        return rec(nn=2 ** (n - 1), kk=k)
+                for x, y in moves:
+                    nr = r + x
+                    nc = c + y
+
+                    if (0 <= nr < 3 and 0 <= nc < 3) or (nr == 3 and nc == 1):
+                        ans += helper(n - 1, nr, nc)
+
+                return ans
+
+        ans = helper(n - 1, 3, 1)
+
+        for i in range(3):
+            for j in range(3):
+                ans += helper(n - 1, i, j)
+
+        return ans
 
 
 def main():
     s = Solution()
-    ans = s.kthGrammar(n=2, k=2)
+    ans = s.knightDialer(n=2)
+
     print(ans)
 
 
